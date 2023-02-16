3791e6c9421606032fa03baf20811f1704ed0c00
Weiner Oliveira <woliveira82@gmail.com>
2023-02-16 22:58:24 +0000

    feat: generate random level

---
+++ game/World.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var borders = Rect2(1, 1, 30, 17)
+
+onready var tile_map = $TileMap
+
+
+func _ready():
+	randomize()
+	generate_level()
+
+
+func generate_level():
+	var walker = Walker.new(Vector2(9, 6), borders)
+	var map = walker.walk(500)
+	walker.queue_free()
+	for location in map:
+		tile_map.set_cellv(location, -1)
+	
+	tile_map.update_bitmask_region(borders.position, borders.end)
+
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		get_tree().reload_current_scene()
