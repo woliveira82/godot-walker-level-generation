884dc8d05b20e4ef5447f6fa62386db150d7827d
Weiner Oliveira <woliveira82@gmail.com>
2023-02-17 22:32:50 +0000

    feat: add simple player

---
+++ game/Player.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+
+
+func _physics_process(_delta):
+	var input = Vector2.ZERO
+	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	move_and_slide(input * 100)
