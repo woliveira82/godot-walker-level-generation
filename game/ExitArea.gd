b5289dce99c92ba8682b005bdce91dacbb4296d7
Weiner Oliveira <woliveira82@gmail.com>
2023-02-17 23:45:28 +0000

    feat: add exit to start a new level

---
+++ game/ExitArea.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+signal leaving_level
+
+
+func _on_ExitArea_body_entered(body):
+	emit_signal("leaving_level")
--- game/World.gd
+++ game/World.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const Player = preload("res://game/Player.tscn")
+const Exit = preload("res://game/ExitArea.tscn")
 
 var borders = Rect2(1, 1, 30, 17)
 
@@ -15,9 +16,16 @@ func _ready():
 func generate_level():
 	var walker = Walker.new(Vector2(15, 8), borders)
 	var map = walker.walk(200)
+	
 	var player = Player.instance()
 	add_child(player)
 	player.position = map.front() * 32
+	
+	var exit = Exit.instance()
+	add_child(exit)
+	exit.position = walker.get_end_room().position * 32
+	exit.connect("leaving_level", self, "reload_level")
+
 	walker.queue_free()
 	for location in map:
 		tile_map.set_cellv(location, -1)
@@ -28,3 +36,7 @@ func generate_level():
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		get_tree().reload_current_scene()
+
+
+func reload_level():
+	get_tree().reload_current_scene()
